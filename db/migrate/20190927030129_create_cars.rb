b82bed90cb48a17a74d7bb1165495966ad98a0f0
Leslee Langlands <leslee@calibrateconsulting.ca>
2019-09-26 20:15:57 -0700

    initial commit

---
+++ db/migrate/20190927030129_create_cars.rb
@@ -0,0 +1,17 @@
+class CreateCars < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cars do |t|
+      t.string :make
+      t.string :model
+      t.string :yerails
+      t.string :generate
+      t.string :scaffold
+      t.string :car
+      t.string :make
+      t.string :model
+      t.integer :year
+
+      t.timestamps
+    end
+  end
+end
